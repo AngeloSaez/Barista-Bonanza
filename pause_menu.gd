5dbff9c876b8ad89b335535b5e4333aefc0d3360
ChaosIncarnate4 <wj0503@gmail.com>
2024-03-14 23:57:43 -0400

    Pause Update

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,17 +1,21 @@
 extends Control
 
+func _ready():
+	hide()
+
+func resume():
+	hide()
+	get_tree().paused = false
+
+func pause():
+	show()
+	get_tree().paused = true
+
 func testPause():
 	if Input.is_action_just_pressed("Pause") and !get_tree().paused:
 		pause()
 	elif Input.is_action_just_pressed("Pause") and get_tree().paused:
 		resume()
-		
-
-func pause():
-	get_tree().paused = true
-
-func resume():
-	get_tree().paused = false
 
 func _on_resume_pressed():
 	resume()
@@ -22,3 +26,6 @@ func _on_restart_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _process(delta):
+	testPause()
